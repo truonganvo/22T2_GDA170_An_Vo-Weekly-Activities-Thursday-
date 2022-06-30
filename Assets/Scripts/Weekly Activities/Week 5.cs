162730f73cb033a4c100d6942c7945ee52f1d149
An Vo <107334075+truonganvo@users.noreply.github.com>
2022-06-30 12:16:26 +1000

    Create A Design Pattern Template

---
+++ Assets/Scripts/Weekly Activities/Week 5.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Week5Activities : MonoBehaviour
+{
+    public new string name;
+    public int phonenumber;
+    public string address;
+    public string email;
+    public string preferredname;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Contact List");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
